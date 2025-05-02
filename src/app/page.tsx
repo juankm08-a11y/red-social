9066d6878278b86a74e7085496790d62f575c455
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-01 21:47:57 -0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="w-full h-full bg-white flex justify-center items-center">
+      <h1 className="font-bold">Red Social</h1>
+    </main>
+  );
+}
