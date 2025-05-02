cb5626e5b123f295019f8cc121c625a6e3386403
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-01 22:59:38 -0500

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-
   const handleMensajes = () => {
     alert("Has recibido un mensaje");
   };
