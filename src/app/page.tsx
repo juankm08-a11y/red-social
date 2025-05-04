0cc9ab83023646dd03d1a862b0497cf5ed334b7c
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-03 20:19:43 -0500

    third commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,33 @@
 "use client";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
-  const handleMensajes = () => {
-    alert("Has recibido un mensaje");
-  };
+  const [contenido, setContenido] = useState("");
+  const handleMensajes = () => alert("Has recibido un mensaje");
 
-  const handleNotificaciones = () => {
-    alert("Has recibido una notificacion");
-  };
+  const handleNotificaciones = () => alert("Has recibido una notificacion");
 
-  const handlePerfil = () => {
-    alert("Has creado tu perfil");
+  const handlePerfil = () => alert("Has creado tu perfil");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/publicaciones`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ contenido }),
+      }
+    );
+
+    if (res.ok) {
+      alert("Publicacion enviada correctamente");
+      setContenido("");
+    } else {
+      alert("Error al enviar publicación");
+    }
   };
   return (
     <main className="w-full h-full bg-white flex flex-col justify-center items-center">
@@ -32,18 +48,21 @@ export default function Home() {
       </header>
       <section>
         <h2 className="font-semibold text-center">Publish</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>
             <input
               type="text"
               placeholder="Que estas pensando......"
               className="border-2 w-49 h-10"
+              value={contenido}
+              onChange={(e) => setContenido(e.target.value)}
             />
           </label>
+          <button type="submit">Publicar</button>
         </form>
       </section>
       <section>
-        <p>Lorem ipsum</p>
+        <h2 className="font-semibold">Juan Muñoz</h2>
       </section>
     </main>
   );
